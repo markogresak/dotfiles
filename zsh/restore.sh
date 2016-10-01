22d7fd66740094398eb3f5e1a90131577cc95f47
Marko Grešak <gresak.marko@gmail.com>
2016-10-01 05:13:46 +0200

    (zsh|restore) remove ~/.oh-my-zsh folder before installing

--- zsh/restore.sh
+++ zsh/restore.sh
@@ -7,6 +7,8 @@ install_zsh_log="install_zsh.log"
 
 function install_zsh {
   if [[ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]]; then
+    # remove ~/.oh-my-zsh folder in case it exists before installing
+    rm -rf "$HOME/.oh-my-zsh"
     echo "~/.oh-my-zsh does not exist. Installing oh-my-zsh now..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> $install_zsh_log
   fi
