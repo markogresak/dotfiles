acc86e6cb247cb304b8b1a3e98e3ed1610c19adf
Marko Grešak <gresak.marko@gmail.com>
2016-10-01 18:34:12 +0200

    (editor|restore) check for vim before trying to install vundle

--- editor/restore.sh
+++ editor/restore.sh
@@ -3,7 +3,9 @@
 install_vundle_log="install_vundle.log"
 
 function install_vundle {
-  if [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
+  if ! hash vim 2>/dev/null; then
+    echo "ERROR: vim is not installed, unable to install vundle without vim."
+  elif [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
     echo "~/.vim/bundle/Vundle.vim does not exist. Installing Vundle now..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> $install_vundle_log
     echo "" >> $install_vundle_log
