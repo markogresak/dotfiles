91a270109130858ec0468a6629e81c3bb505d88d
Marko Grešak <gresak.marko@gmail.com>
2016-10-01 05:07:53 +0200

    (zsh|restore) fix test for zsh to check for oh-my-zsh.sh

--- zsh/restore.sh
+++ zsh/restore.sh
@@ -6,7 +6,7 @@ echo 'Please restore ~/.secretrc manually.'
 install_zsh_log="install_zsh.log"
 
 function install_zsh {
-  if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  if [[ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]]; then
     echo "~/.oh-my-zsh does not exist. Installing oh-my-zsh now..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> $install_zsh_log
   fi
