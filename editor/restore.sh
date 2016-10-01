7ff119782e229a90160cba2c1c698929b257187e
Marko Grešak <gresak.marko@gmail.com>
2016-10-01 04:11:02 +0200

    (editor|restore) fix vim +PluginInstall redirect

--- editor/restore.sh
+++ editor/restore.sh
@@ -7,7 +7,7 @@ function install_vundle {
     echo "~/.vim/bundle/Vundle.vim does not exist. Installing Vundle now..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> $install_vundle_log
     echo "" >> $install_vundle_log
-    vim +PluginInstall +qall &>> $install_vundle_log
+    vim +PluginInstall +qall >> $install_vundle_log 2>&1
   fi
 }
 
