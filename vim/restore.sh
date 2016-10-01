8af3afe5d085a3e87f0b6736b04970271569e4d7
Marko Grešak <gresak.marko@gmail.com>
2016-10-01 02:30:33 +0200

    (fix) change ~ path to $HOME
    The ~ wasn't evaluated correctly

--- vim/restore.sh
+++ vim/restore.sh
@@ -3,7 +3,8 @@
 install_vundle_log="install_vundle.log"
 
 function install_vundle {
-  if [[ ! -d "~/.vim/bundle/Vundle.vim" ]]; then
+  if [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]]; then
+    echo "~/.vim/bundle/Vundle.vim does not exist. Installing Vundle now..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> $install_vundle_log
     echo "" >> $install_vundle_log
     vim +PluginInstall +qall &>> $install_vundle_log
--- zsh/restore.sh
+++ zsh/restore.sh
@@ -6,7 +6,8 @@ echo 'Please restore ~/.secretrc manually.'
 install_zsh_log="install_zsh.log"
 
 function install_zsh {
-  if [[ ! -d "~/.oh-my-zsh" ]]; then
+  if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+    echo "~/.oh-my-zsh does not exist. Installing oh-my-zsh now..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> $install_zsh_log
   fi
 }
