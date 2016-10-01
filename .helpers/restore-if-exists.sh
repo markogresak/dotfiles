fdc17c534ffca06269ad73a04471fa4fb24b8a39
Marko Grešak <gresak.marko@gmail.com>
2016-10-01 02:45:37 +0200

    (fix) bug with restore script cp command

--- .helpers/restore-if-exists.sh
+++ .helpers/restore-if-exists.sh
@@ -4,9 +4,9 @@ source_file_path="$(basename $1)"
 target_file_path="$1"
 
 if [ -f "$source_file_path" ]; then
-  # make sure full path exists before trying to copy file 
+  # make sure full path exists before trying to copy file
   mkdir -p $(dirname "$target_file_path")
-  cp -r "$target_file_path"
+  cp -r "$source_file_path" "$target_file_path"
   echo "Updated $target_file_path."
   exit 0
 else
