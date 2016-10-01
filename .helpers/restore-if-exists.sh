f5560421d7dd8e543ad38a6c4ef59e25c371aa97
Marko Grešak <gresak.marko@gmail.com>
2016-10-01 20:07:50 +0200

    (log) fix name of config_files operation

--- .helpers/restore-if-exists.sh
+++ .helpers/restore-if-exists.sh
@@ -7,9 +7,9 @@ if [ -f "$source_file_path" ]; then
   # make sure full path exists before trying to copy file
   mkdir -p $(dirname "$target_file_path")
   cp -r "$source_file_path" "$target_file_path"
-  eval ../.helpers/log.sh "config files" "Restored $source_file_path at $target_file_path."
+  eval ../.helpers/log.sh "config_files" "Restored $source_file_path at $target_file_path."
   exit 0
 else
-  eval ../.helpers/log.sh "config files" "File $source_file_path does not exist, skip restoring." "error"
+  eval ../.helpers/log.sh "config_files" "File $source_file_path does not exist, skip restoring." "error"
   exit 1
 fi
--- .helpers/update-if-exists.sh
+++ .helpers/update-if-exists.sh
@@ -4,9 +4,9 @@ file_path="$1"
 
 if [ -f "$file_path" ]; then
   cp -r "$file_path" ./
-  eval ../.helpers/log.sh "config files" "Updated $file_path."
+  eval ../.helpers/log.sh "config_files" "Updated $file_path."
   exit 0
 else
-  eval ../.helpers/log.sh "config files" "File $file_path does not exist, skip updating." "error"
+  eval ../.helpers/log.sh "config_files" "File $file_path does not exist, skip updating." "error"
   exit 1
 fi
