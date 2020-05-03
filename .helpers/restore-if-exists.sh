f1cf8b3f79c41ed3d1a8e3b8d248f4a7fcd4294d
Marko Grešak <marko@gresak.io>
2020-05-03 21:57:59 +0200

    Do not create an additional target dir when copying config

--- .helpers/restore-if-exists.sh
+++ .helpers/restore-if-exists.sh
@@ -6,7 +6,7 @@ target_file_path="$1"
 if [ -f "$source_file_path" ] || [ -d "$source_file_path" ]; then
   # make sure full path exists before trying to copy file
   mkdir -p $(dirname "$target_file_path")
-  cp -r "$source_file_path" "$target_file_path"
+  cp -r --no-target-directory "$source_file_path" "$target_file_path"
   eval ../.helpers/log.sh "config_files" "Restored $source_file_path at $target_file_path."
   exit 0
 else
