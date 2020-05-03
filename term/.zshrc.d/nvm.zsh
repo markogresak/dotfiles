db9aa263d7cf46ea19f581bca3adf288c56f4c13
Marko Grešak <marko@gresak.io>
2020-05-03 21:53:34 +0200

    Update the approach to add node from nvm to path

--- term/.zshrc.d/nvm.zsh
+++ term/.zshrc.d/nvm.zsh
@@ -1,7 +1,3 @@
-# export NVM_DIR="$HOME/.nvm"
-# not using $(brew --prefix nvm) because it's slow
-# [ -s "$BREW_PATH/opt/nvm/nvm.sh" ] && . "$BREW_PATH/opt/nvm/nvm.sh" --no-use
-
 # disabled because it's slow, do a manual `nvm use` where it applies
 # function nvm_use_on_cd {
 #   nvm use > /dev/null 2>&1 || true
@@ -9,5 +5,8 @@
 # # run on every cd
 # chpwd_functions=(${chpwd_functions[@]} "nvm_use_on_cd")
 
-# A workaround to setting node path because `nvm use` is slow
-# PATH="$PATH:$NVM_DIR/versions/node/v12.16.1/bin"
+export NVM_DIR="$HOME/.nvm"
+
+# A workaround to sourcing nvm script path because `nvm use` is slow
+NODE_VERSION="$(cat "$NVM_DIR/alias/lts/*" | tail -1)"
+export PATH="$PATH:$NVM_DIR/versions/node/$NODE_VERSION/bin"
